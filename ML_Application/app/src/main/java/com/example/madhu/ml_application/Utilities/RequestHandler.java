44804483f7bd3b4be1243a595c1bad0ebc4da820
Madhu <s.madhu.akp@gmail.com>
2017-04-04 15:31:10 +0530

    Image upload

---
+++ ML_Application/app/src/main/java/com/example/madhu/ml_application/Utilities/RequestHandler.java
@@ -0,0 +1,74 @@
+package com.example.madhu.ml_application.Utilities;
+ import org.json.JSONException;
+ import org.json.JSONObject;
+
+ import java.io.BufferedReader;
+ import java.io.BufferedWriter;
+ import java.io.InputStreamReader;
+ import java.io.OutputStream;
+ import java.io.OutputStreamWriter;
+ import java.io.UnsupportedEncodingException;
+ import java.net.HttpURLConnection;
+ import java.net.URL;
+ import java.net.URLEncoder;
+ import java.util.HashMap;
+ import java.util.Map;
+
+ import javax.net.ssl.HttpsURLConnection;
+
+
+
+public class RequestHandler {
+
+    public String sendPostRequest(String requestURL,
+                                  HashMap<String, String> postDataParams) {
+
+        URL url;
+
+        StringBuilder sb = new StringBuilder();
+        try {
+            url = new URL(requestURL);
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(15000);
+            conn.setConnectTimeout(15000);
+            conn.setRequestMethod("POST");
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+
+
+            OutputStream os = conn.getOutputStream();
+            BufferedWriter writer = new BufferedWriter(
+                    new OutputStreamWriter(os, "UTF-8"));
+            writer.write(convertMapToJson(postDataParams).toString());
+
+            writer.flush();
+            writer.close();
+            os.close();
+            int responseCode = conn.getResponseCode();
+
+            if (responseCode == HttpsURLConnection.HTTP_OK) {
+                BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                sb = new StringBuilder();
+                String response;
+                while ((response = br.readLine()) != null){
+                    sb.append(response);
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return sb.toString();
+    }
+
+
+    private JSONObject convertMapToJson(HashMap<String,String> data)throws JSONException
+    {
+        JSONObject obj=new JSONObject();
+        obj.put("name",data.get("name"));
+        obj.put("pwd",data.get("pwd"));
+
+        return obj;
+    }
+}
